a1cc683eff94405018461be1592566e6d2f7fded
HeSH <neogate@sogang.ac.kr>
2021-11-13 22:56:05 +0900

    Create yee.cpp

---
+++ yee.cpp
@@ -0,0 +1,22 @@
+#define yee <bits/stdc++.h>
+#define yeee using
+#define yeeee namespace
+#define yeeeee std;
+typedef int yeeeeee;
+#define yeeeeeee main()
+#define yeeeeeeee {
+#define yeeeeeeeee cout
+#define yeeeeeeeeee }
+#define yeeeeeeeeeee <<
+#define yeeeeeeeeeeee endl
+#define yeeeeeeeeeeeee "yee"
+#define yeeeeeeeeeeeeee ;
+#define yeeeeeeeeeeeeeee return
+#define yeeeeeeeeeeeeeeee 0;
+
+#include yee
+yeee yeeee yeeeee 
+yeeeeee yeeeeeee yeeeeeeee
+yeeeeeeeee yeeeeeeeeeee yeeeeeeeeeeeee yeeeeeeeeeee yeeeeeeeeeeee yeeeeeeeeeeeeee
+yeeeeeeeeeeeeeee yeeeeeeeeeeeeeeee
+yeeeeeeeeee
